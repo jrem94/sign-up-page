e8cfc0aa44eb116f0c1c5feb4fdecb549b673dac
Joshua Morris <jrem94@live.com>
2018-07-14 15:42:04 -0500

    .js and .html v2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let emailInput = document.getElementById('#collection');
 
-document.querySelector('div.active-form').('submit', function () {
+document.querySelector('div.active-form').addEventListener('button', function(value) {
 
     console.log(nameInput.value); 
 });
+
